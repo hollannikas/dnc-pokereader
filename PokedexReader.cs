58aac831751f24127f3414f0d77ae32915102656
hollannikas <hollannikas@users.noreply.github.com>
2016-10-15 13:06:57 +0300

    Initial commit

---
+++ PokedexReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace PokedexReader
+{
+    internal class Pokemon
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return "Pokemón #" + ID + " (" + Name + ") Height: " + Height + ", Weight: " + Weight;
+        }
+    }
+
+    class Pokedex
+    { 
+        public static void Main()
+        {
+            List<Pokemon> pokemons = new List<Pokemon>();
+            foreach( int ID in Enumerable.Range(1, 25) ) {
+                pokemons.Add(FetchPokemon(ID).Result);
+            }
+            
+            pokemons.ForEach(pokemon => {
+                Console.WriteLine(pokemon);
+            });
+        }
+
+        public static async Task<Pokemon> FetchPokemon(int id)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string json = await client.GetStringAsync("http://pokeapi.co/api/v2/pokemon/" + id);
+                Pokemon result = JsonConvert.DeserializeObject<Pokemon>(json);   
+                return result;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Gets pokémon #1 - #25 from [pokéapi](https://pokeapi.co/) in C# (.net core)
+
+Uses [Json.NET](https://github.com/JamesNK/Newtonsoft.Json) to deserialize JSON
